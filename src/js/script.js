a46231712a6ab3ca0a9324ca034639e756396f58
Sg1azz <novus8524@gmail.com>
2020-04-01 09:57:43 +0300

    add map

--- src/js/script.js
+++ src/js/script.js
@@ -1,16 +1,37 @@
-const slider = tns({
-	container: '.carousel__inner',
-    items: 1,
-    slideBy: 'page',
-	autoplay: false,
-	controls: false,
-	nav: false
-});
+  
+$(document).ready(function(){
+    $('.carousel__inner').slick({
+        speed: 1200,
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/left.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/right.png"></button>',
+        responsive: [
+            {
+                breakpoint: 992,
+                settings: {
+                    dots: true,
+                    arrows: false
+                }
+            }
+        ]
+    });
+    
+    $('ul.catalogue__tabs').on('click', 'li:not(.catalogue__tab_active)', function() {
+        $(this)
+          .addClass('catalogue__tab_active').siblings().removeClass('catalogue__tab_active')
+          .closest('div.container').find('div.catalogue__content').removeClass('catalogue__content_active').eq($(this).index()).addClass('catalogue__content_active');
+    });
 
-document.querySelector('.prev').addEventListener('click', function () {
-	slider.goTo('prev');
-});
+    function toggleSlide(item) {
+        $(item).each(function(i) {
+            $(this).on('click', function(e) {
+                e.preventDefault();
+                $('.catalogue-item__content').eq(i).toggleClass('catalogue-item__content_active');
+                $('.catalogue-item__list').eq(i).toggleClass('catalogue-item__list_active');
+            })
+        });
+    };
 
-document.querySelector('.next').addEventListener('click', function () {
-	slider.goTo('next');
+    toggleSlide('.catalogue-item__link');
+    toggleSlide('.catalogue-item__back');
 });
